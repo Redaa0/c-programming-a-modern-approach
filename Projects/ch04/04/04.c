5368fd0a5987ecbc28ba5c0950119fa752d7523f
Reda <redamoutaouakkil99@gmail.com>
2023-03-20 11:15:01 +0000

    solved

--- Projects/ch04/04/04.c
+++ Projects/ch04/04/04.c
@@ -2,13 +2,17 @@
 
 int main(void)
 {
+    // Prompt the user to enter a number
     int n, n1, n2, n3, n4, n5;
     printf("Enter a number between 0 and 32767: ");
     scanf("%d", &n);
 
     n5 = n % 8;
     n4 = (n / 8) % 8;
-    n3 =  n / 80;
+    n3 = (n / 64) % 8;
+    n2 = (n / 512) % 8;
+    n1 = (n / 4096) % 8;
 
-    printf("In octal, your number is: %d%d%d\n", n5, n4, n3);
+    // Display the number converted to octal
+    printf("In octal, your number is: %d%d%d%d%d\n", n1, n2, n3, n4, n5);
 }
