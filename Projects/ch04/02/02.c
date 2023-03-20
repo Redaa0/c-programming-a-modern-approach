6131bfabb584a2f8e571db404d58bf4c2c53f334
Reda <redamoutaouakkil99@gmail.com>
2023-03-20 10:11:54 +0000

    solved

---
+++ Projects/ch04/02/02.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // Prompt the user to enter the number
+    int n, n3, n2, n1;
+    printf("Enter a three-digit number: ");
+    scanf("%d", &n);
+
+    n3 = n % 10;
+    n2 = (n / 10) % 10;
+    n1 = n / 100;
+    
+    // Print the number reversed
+    printf("The reversal is: %d%d%d\n", n3, n2, n1);   
+}
