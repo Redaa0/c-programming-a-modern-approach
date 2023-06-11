bfa0ea9bfeac92a56983f1c42e13c8d69ef7b718
Reda <redamoutaouakkil99@gmail.com>
2023-06-11 07:13:24 +0100

    02

---
+++ Projects/ch05/02/02.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main(void)
+{
+    int hour, minute, time;
+
+    // Prompt the user to enter the 24-hour time
+    printf("Enter a 24-hour time: ");
+    scanf("%d:%d", &hour, &minute);
+
+    // Print the time in 12-hour format
+    if (hour > 0 && hour <= 11)
+        printf("Equivalent 12-hour time: %.1d:%.2d AM\n", hour, minute);
+        else if (hour == 0)
+        {
+            printf("Equivalent 12-hour time: 12:%.2d AM\n", minute);
+        }
+    else if(hour > 12 && hour <= 23)
+        printf("Equivalent 12-hour time: %.1d:%.2d PM\n", hour - 12, minute);
+        else if (hour == 12)
+            printf("Equivalent 12-hour time: 12:%.2d PM\n", minute);
+
+    return (0);
+}
