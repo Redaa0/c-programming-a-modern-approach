bfca9634a3b494f5633f2007d2b975eddd765e7a
Reda <redamoutaouakkil99@gmail.com>
2023-09-24 06:49:16 +0100

    solved

---
+++ Projects/ch07/06/06.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    printf("Sizeof(int): %ld\n", sizeof(int));
+    printf("Sizeof(short): %ld\n", sizeof(short));
+    printf("Sizeof(long): %ld\n", sizeof(long));
+    printf("Sizeof(float): %ld\n", sizeof(float));
+    printf("Sizeof(double): %ld\n", sizeof(double));
+    printf("Sizeof(long double): %ld\n", sizeof(long double));
+}
